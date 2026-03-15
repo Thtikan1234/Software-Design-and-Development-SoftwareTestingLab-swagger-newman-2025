09b16d5f2a77fbf654820bacb6a5ff976e0b93bd
Thtikan1234 <68030072@kmitl.ac.th>
2026-03-15 17:54:59 +0700

    11

---
+++ server.js
@@ -0,0 +1,3 @@
+// เพิ่มหลังบรรทัด require เดิม
+const swaggerJsdoc = require('swagger-jsdoc');       // อ่าน JSDoc comment → สร้าง spec
+const swaggerUi    = require('swagger-ui-express');   // serve spec เป็น Interactive UI
